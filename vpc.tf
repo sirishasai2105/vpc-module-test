4081e52f04fd343cf7e4b9fe02d2ddae6a2fdf7a
siri123 <saisirishayarra692@gmail.com>
2024-09-27 10:50:59 +0530

    add files

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,5 @@ module "vpc" {
     private_subnet_cidrs = var.private_subnet_cidrs_block
     database_subnet_cidrs = var.database_subnet_cidr_block
     is_peering_required = var.is_peering_required
-    #destination_cidr_block = var.dest_cidr_block
-
 }
 
